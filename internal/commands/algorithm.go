c364109a32dbf925f9940e7f441d457672bed56b
Oleksandr Cherevkov <oleksandrcherevkov@gmail.com>
2024-11-21 11:47:15 +0100

    use cast-128

--- internal/commands/algorithm.go
+++ internal/commands/algorithm.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"strconv"
 
 	"github.com/oleksandrcherevkov/cryptography/internal/console"
 	"github.com/oleksandrcherevkov/cryptography/internal/crypto"
@@ -18,6 +20,7 @@ func (s *AlgorithmCommand) Exec() (Command, error) {
 	fmt.Println("1. Cesar algorithm")
 	fmt.Println("2. Decrypt")
 	fmt.Println("3. Gamma")
+	fmt.Println("4. CAST-128")
 	response, err := console.GetString()
 	if err != nil {
 		return nil, err
@@ -59,6 +62,38 @@ func (s *AlgorithmCommand) Exec() (Command, error) {
 			input:     s.input,
 			output:    s.output,
 		}, nil
+	case "4":
+		fmt.Print("Input key hex -> ")
+		key, err := getCastKey()
+		if err != nil {
+			return nil, err
+		}
+		cast := crypto.NewCast(key)
+		return &ActionCommand{
+			algorithm: cast,
+			input:     s.input,
+			output:    s.output,
+		}, nil
 	}
 	return ExitCommand{}, nil
+}
+
+func getCastKey() ([16]byte, error) {
+	var key [16]byte
+	keyString, err := console.GetString()
+	if err != nil {
+		return key, err
+	}
+	if len(keyString) != 32 {
+		return key, errors.New("key not right size of 32 hex values")
+	}
+	for i := 0; i < 32; i += 2 {
+		byteString := keyString[i : i+2]
+		number, err := strconv.ParseUint(byteString, 16, 8)
+		if err != nil {
+			return key, err
+		}
+		key[i/2] = byte(number)
+	}
+	return key, nil
 }
